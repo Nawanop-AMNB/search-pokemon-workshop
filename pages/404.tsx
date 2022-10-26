3da02758f34ee7737a513dc2d5489b8235bcecda
Nawanop <ka.nawanop@gmail.com>
2022-10-27 05:04:23 +0700

    fix go back button not go to homepage

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import NotFoundView from '../src/components/NotFoundView';
+import router from 'next/router';
 
 function NotFoundPage() {
+  const onGoHome = () => {
+    router.push('/');
+  };
   return (
     <div className='h-full w-full'>
       <NotFoundView
         bottom={
-          <button className='text-white text-xl bg-purple-400 rounded-md p-4 pt-2 pb-2 w-40'>
+          <button
+            onClick={onGoHome}
+            className='text-white text-xl font-semibold bg-purple-400 hover:bg-purple-500 focus:bg-purple-700 rounded-md p-4 pt-2 pb-2 w-40'
+          >
             Go Back
           </button>
         }
